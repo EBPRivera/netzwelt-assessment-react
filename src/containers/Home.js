358c723d1b6e1e8771ce00ab451dd8b40472f83f
Eduard Bryan P. Rivera <edubprivera@yahoo.com>
2023-08-20 13:24:13 +0800

    Add territories display in homepage

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,80 @@
+import _ from "lodash";
+import { useEffect, useState } from "react";
+import { Container, Row } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+import useAxiosInstance from "../hooks/useAxiosInstance";
+import parseTerritories from "../helpers/parseTerritories";
+import Errors from "../components/Errors";
+
 const Home = () => {
-  return <h1>Home Placeholder</h1>;
+  const [errors, setErrors] = useState([]);
+  const [territories, setTerritories] = useState([]);
+  const user = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const axiosInstance = useAxiosInstance();
+
+  useEffect(() => {
+    const fetchTerritories = async () => {
+      await axiosInstance
+        .get("/territories/all")
+        .then((response) => {
+          setTerritories(parseTerritories(response.data.data));
+        })
+        .catch((e) => {
+          setErrors([e.message]);
+        });
+    };
+
+    if (_.isUndefined(user.roles) || _.isEmpty(user.roles)) {
+      navigate("/account/login");
+    } else {
+      fetchTerritories();
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleCloseError = (key) => {
+    setErrors((errors) =>
+      _.remove(errors, (error, index) => _.isEqual(key, index))
+    );
+  };
+
+  const renderErrors = () => {
+    if (_.isEmpty(errors)) return;
+
+    return (
+      <Row>
+        <Errors errors={errors} onClose={handleCloseError} />
+      </Row>
+    );
+  };
+
+  const renderTerritories = (territories) => {
+    if (_.isEmpty(territories)) return;
+
+    return (
+      <ul>
+        {_.map(territories, (territory, key) => {
+          return (
+            <li key={key}>
+              {territory.name}
+              {renderTerritories(territory.children)}
+            </li>
+          );
+        })}
+      </ul>
+    );
+  };
+
+  return (
+    <Container className="text-center pt-3">
+      {renderErrors()}
+      <Row className="text-start">{renderTerritories(territories)}</Row>
+    </Container>
+  );
 };
 
 export default Home;
