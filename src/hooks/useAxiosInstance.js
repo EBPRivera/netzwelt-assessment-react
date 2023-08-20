f86f1f7de3b97428e131850fcc33c706890860f7
Eduard Bryan P. Rivera <edubprivera@yahoo.com>
2023-08-20 17:16:12 +0800

    Update base url of axios requests to proxy web server

--- src/hooks/useAxiosInstance.js
+++ src/hooks/useAxiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const useAxiosInstance = () => {
   const axiosInstance = axios.create({
-    baseURL: "https://netzwelt-devtest.azurewebsites.net/",
+    baseURL: process.env.REACT_APP_BASE_API_URL || "http://localhost:8080",
   });
 
   return axiosInstance;
