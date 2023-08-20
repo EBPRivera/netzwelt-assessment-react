556d9cf178e9930c6eaf1815eaa0d3ab35ceeeec
Eduard Bryan P. Rivera <edubprivera@yahoo.com>
2023-08-20 17:31:27 +0800

    Refactor auth links in navigation bar to handle logout

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,8 +1,29 @@
 import { Nav, Navbar, Container } from "react-bootstrap";
 import { useNavigate } from "react-router";
+import { useDispatch } from "react-redux";
+
+import useAuthorized from "../hooks/useAuthorized";
+import { logout } from "../features/user";
 
 const NavigationBar = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const isAuthorized = useAuthorized();
+
+  const handleAuthAction = () => {
+    if (isAuthorized) {
+      dispatch(logout());
+    }
+    navigate("/account/login");
+  };
+
+  const renderAuthLinks = () => {
+    return (
+      <Nav.Link onClick={handleAuthAction}>
+        {isAuthorized ? "Logout" : "Login"}
+      </Nav.Link>
+    );
+  };
 
   return (
     <Navbar expand="lg" className="bg-body-secondary">
@@ -11,10 +32,9 @@ const NavigationBar = () => {
         <Navbar.Collapse>
           <Nav>
             <Nav.Link onClick={() => navigate("/")}>Home</Nav.Link>
-            <Nav.Link onClick={() => navigate("/account/login")}>
-              Login
-            </Nav.Link>
           </Nav>
+          <Navbar.Collapse />
+          <Nav>{renderAuthLinks()}</Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
